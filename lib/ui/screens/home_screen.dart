1987b6a605229a6c208eae1aad256e5f5fe04e27
joe02 <9836Joel>
2021-07-05 16:57:10 -0300

    CREATE_CONTACT - Added Fancy floating action button to lead to creating screen.

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:eng_soft_contacts_list/ui/components/FancyFab.dart';
 import 'package:eng_soft_contacts_list/ui/screens/login_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
@@ -19,49 +20,61 @@ class HomeScreenState extends State<HomeScreen> {
       future: loadUser(),
       builder: (context, snapshot) {
         return Scaffold(
-          appBar: AppBar(
-            title: Text("Olá, ${getUserName()}"),
-            actions: [
-              IconButton(
-                  onPressed: () {
-                    FirebaseAuth.instance.signOut();
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(builder: (context) => LoginScreen()),
-                    );
-                  },
-                  icon: Icon(Icons.logout))
-            ],
+          appBar: buildAppBar(),
+          floatingActionButton: FancyFab(
+            onPressed: () {
+              ///TODO Navigate to create contact
+            },
+            icon: Icons.add,
+            tooltip: 'Adicionar contato ou grupo.',
           ),
           body: Center(
-              child: Container(
-                child: Column(
-                  children: [
-                    Container(
-                      padding: EdgeInsets.only(
-                          left: 2.5, right: 2.5, bottom: 5.5),
-                      width: screenWidth,
-                      height: screenHeight * 0.085,
-                      child: Card(
-                        elevation: 5.0,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(15)),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            Padding(
-                              padding: EdgeInsets.only(right: 8.0),
-                              child: Icon(Icons.search),
-                            )
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              )),
+            child: Container(
+              child: Column(
+                children: [buildSearchBar(screenWidth, screenHeight)],
+              ),
+            ),
+          ),
         );
-      }
+      },
+    );
+  }
+
+  buildAppBar() {
+    return AppBar(
+      title: Text("Olá, ${getUserName()}"),
+      actions: [
+        IconButton(
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => LoginScreen()),
+              );
+            },
+            icon: Icon(Icons.logout))
+      ],
+    );
+  }
+
+  buildSearchBar(screenWidth, screenHeight) {
+    return Container(
+      padding: EdgeInsets.only(left: 2.5, right: 2.5, bottom: 5.5),
+      width: screenWidth,
+      height: screenHeight * 0.085,
+      child: Card(
+        elevation: 5.0,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(right: 8.0),
+              child: Icon(Icons.search),
+            )
+          ],
+        ),
+      ),
     );
   }
 
@@ -75,7 +88,8 @@ class HomeScreenState extends State<HomeScreen> {
   }
 
   String? getUserName() {
-    if (FirebaseAuth.instance.currentUser!.displayName == null || FirebaseAuth.instance.currentUser!.displayName == "") {
+    if (FirebaseAuth.instance.currentUser!.displayName == null ||
+        FirebaseAuth.instance.currentUser!.displayName == "") {
       return FirebaseAuth.instance.currentUser!.email;
     } else {
       FirebaseAuth.instance.currentUser!.displayName;
